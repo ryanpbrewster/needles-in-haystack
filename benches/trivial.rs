01b2fa4d770f60996938e1d6c8c2ddaa6448b3dd
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-07-24 22:52:37 -0700

    Add benchmarks and a Rabin-Karp implementation

---
+++ benches/trivial.rs
@@ -0,0 +1,35 @@
+#[macro_use]
+extern crate criterion;
+extern crate needles_in_haystack;
+
+use criterion::Criterion;
+
+use needles_in_haystack::{rabin_karp::RabinKarpHaystack, window::WindowHaystack, Haystack};
+
+fn needle() -> Vec<u8> {
+    let mut buf = vec![0; 99];
+    buf.push(1);
+    buf
+}
+fn haystack() -> Vec<u8> {
+    let mut buf = vec![0; 999];
+    buf.push(1);
+    buf
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("window", |b| {
+        let h = WindowHaystack::new(haystack());
+        let n = needle();
+        b.iter(|| h.contains(&n))
+    });
+
+    c.bench_function("rabin-karp", |b| {
+        let h = RabinKarpHaystack::new(haystack());
+        let n = needle();
+        b.iter(|| h.contains(&n))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
